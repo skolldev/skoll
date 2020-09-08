35a28ae3aea727f38befb01ce93e42d2ee0d5a49
Alexander May <alexander.may@spendit.de>
2020-09-08 15:28:38 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import Head from "next/head"
+import Head from "next/head";
 
 export const Home = (): JSX.Element => (
   <div>
     <Head>
       <title>Skoll</title>
       <link rel="icon" href="/favicon.ico" />
-      <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
-    <div className="w-full"></div>
+    <div className="w-full" />
   </div>
-)
+);
 
-export default Home
+export default Home;
